afb6693529b7caf2a769da303780ecc25cf92bd9
Emerson Connelly <emerconn7@gmail.com>
2021-02-03 03:15:30 -0600

    add unzip and re-encode

--- fetch_data.py
+++ fetch_data.py
@@ -2,9 +2,11 @@
 import requests
 import urllib.request
 import os
+import zipfile
+from pathlib import Path
 from bs4 import BeautifulSoup
 
-# set & request SEC Fails-To-Deliver website
+# request SEC Fails-To-Deliver website
 result = requests.get("https://www.sec.gov/data/foiadocsfailsdatahtm")
 
 # ensure website is accessible, or exit with error code
@@ -18,45 +20,84 @@ else:
 # parse and process the website content
 soup = BeautifulSoup(result.content, 'lxml')
 
-# save only the FTD file links to a list and file as (descriptor, url)
-url_list = "url_list.txt"
-with open(url_list, "w") as f:
-    urls = []
+# save zips to a file and a list variable, as (descriptor, url)
+total_urls = []
+counter = 0
+with open("total_url_list.txt", "w") as f:
     for tag in soup.find_all("a"):
         if "half" in tag.text:
-            urls.append((str(tag).split('>')[1].split('<')[0], "https://www.sec.gov" + tag.attrs['href']))
-            f.write(str((str(tag).split('>')[1].split('<')[0], "https://www.sec.gov" + tag.attrs['href'])) +"\n")
-f.close
-print(f"List of all URLs saved to {url_list}\n")
+            total_urls.append((str(tag).split('>')[1].split('<')[0], "https://www.sec.gov" + tag.attrs['href']))
+            f.write(str(total_urls[counter]) + "\n")
+            counter += 1
+f.close()
+print(f"List of all URLs saved to 'url_list.txt'\n")
 
-# sepereate file desriptions and links (not used)
-# file_desc = [url[0] for url in urls]
-# file_url = [url[1] for url in urls]
+# check which zips are not downloaded, add them to list variable
+urls_to_download = []
+for url in total_urls:
+    zip_path = Path("zips/" + str(url[0]))
+    if zip_path.exists() != True:
+        urls_to_download.append(url)
 
-# prompt user with total number of files
-total_files = len(urls)
-print(f"Preparing to download {total_files} .zip files\n")
-
-# ask for download confirmation
-print("Initiate download?")
-user_conf = input("type 'yes' to continue: ")
-if user_conf == "yes":
-    print("Initiating download\n")
+# are there new downloads?
+if len(urls_to_download) > 0:
+    # save zips to download to list
+    counter = 0
+    with open("new_url_list.txt", "w") as f:
+        for url in urls_to_download:
+            f.write(str(urls_to_download[counter]) + "\n")
+            counter += 1
+    f.close()
+    # download confirmation from user
+    print(f"New zip file(s) available: {len(urls_to_download)}")
+    print("See 'new_url_list.txt'")
+    print("Initiate download?")
+    user_conf = input("Type 'yes' to continue: ")
+    if user_conf == "yes":
+        print("Starting download")
+        # create zips folder
+        path = "zips"
+        try:
+            os.makedirs(path, exist_ok = True)
+            print(f".zips will be saved to '{path}' directory\n")
+        except OSError as error:
+            print(f"ERROR, {path} not created")
+        # download zips
+        for url in urls_to_download:
+            urllib.request.urlretrieve(url[1], path + "/" + url[0])
+            print(f"Downloaded: {url[0]}")
+        print("\nDownload finished!\n")
+        # begin unzip process
+        print("Starting unzip process")
+        # create unzips folder
+        path = "data"
+        try:
+            os.makedirs(path, exist_ok = True)
+            print(f".zips will be unzipped to '{path}' directory\n")
+        except OSError as error:
+            print(f"ERROR, {path} not created")
+        # unzip the zips, prepare to encode
+        to_encode = []
+        for url in urls_to_download:
+            with zipfile.ZipFile("zips/" + url[0], "r") as zfile:
+                to_encode += zfile.namelist()
+                zfile.extractall("data/")
+            f.close()
+            print("Unzipped: " + url[0])
+        print("\nUnzip finished!\n")
+        # begin re-encode to utf8
+        print("Starting re-encode process\n")
+        for i in to_encode:
+            print(f"Encoding: {i}")
+            with open(f"data/{i}", "r", encoding="iso8859_14") as f:
+                data = f.read()
+            f.close()
+            with open(f"data/{i}", "w", encoding="utf8") as f:
+                f.write(data)
+            f.close()
+        print("\nEncoding finished!")
+    else:
+        print("Exiting!")
+        exit()
 else:
-    print("Exiting!")
-    exit()
-
-# create directory for data .zip files
-data_path = "zips"
-try:
-    os.makedirs(data_path, exist_ok = True)
-    print(f".zips will be downloaded to '{data_path}' directory\n")
-except OSError as error:
-    print(f"ERROR, {data_path} not created")
-
-# fetch all data .zip files
-for url in urls:
-    urllib.request.urlretrieve(url[1], data_path + "/" + url[0])
-    print(f"Downloaded: {url[0]}")
-
-print("\nDownload finished!")
+    print("Files are current.")
